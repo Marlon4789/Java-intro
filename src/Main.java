70859df8a7faa0954e644fecb4269ff808d49a03
Marlon4789 <cuartasm844@gmail.com>
2024-04-07 12:41:39 -0500

    first comment

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        String name = "Toby";
+        int age= 28;
+        System.out.println("My names is " + name + " and I'm " + age + " years old.");
+
+        // tipos de datos numericos
+
+        // byte: 1 byte short: 2 bytes int: 4 bytes double : 4 bytes long: 8 bytes
+
+        int n = 1234567890;
+        long mL = 123456789014L;
+
+        float nF = 12.56F;
+        double nD = 4545.78787878;
+    }
+}
